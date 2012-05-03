5b71bad0e7d3c15a62af115218cd47b9d500965b
gabrielnatucci@gmail.com <gabrielnatucci@gmail.com@acc8d0b1-88b5-9f1b-88f6-e1774db1a6c6>
2012-05-03 14:28:01 +0000

    Adicionado arquivos de utilização do UDP

--- projeto2/compila.sh
+++ projeto2/compila.sh
@@ -1,2 +1,2 @@
-gcc -g beejClient.c tempo.* mm.* str.* vector.* -o client.o
-gcc -g beejServer.c tempo.* mm.* str.* vector.* resposta.* -o server.o
+gcc -g listener.c tempo.* mm.* str.* vector.* -o listener.o
+gcc -g talker.c tempo.* mm.* str.* vector.* resposta.* -o talker.o
